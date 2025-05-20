6a810075896dc7efae5ae0eb087b5e3a75fb55e9
abhishek636 <bhattabhishek636@gmail.com>
2025-05-20 17:37:22 +0530

    Add Some new sections

--- app/components/home/EverythingIn2.tsx
+++ app/components/home/EverythingIn2.tsx
@@ -5,7 +5,7 @@ export function EverythingIn2() {
     <div className="md:py-16 py-8">
         <div className="max-w-[1180px] mx-auto">
             <h1 className=" font-500 mx-auto text-white leading-none pt-5 z-50">Automatically Updated</h1>
-            <p className="max-w-[450px] text-white text-sm pt-1">Everything - companies, people, and more - is enriched with hundreds of datapoints from a rich global and personal knowledge graph. Plus any property can be updated automatically when there's new email or meeting activity.</p>
+            <p className="max-w-[450px] text-white text-sm pt-1">Everything - companies, people, and more - is enriched with hundreds of datapoints from a rich global and personal knowledge graph. Plus any property can be updated automatically when there&#39;s new email or meeting activity.</p>
         </div>
       
         <div className="grid grid-cols-3 place-items-center gap-4 px-2 pt-10 text-center">
